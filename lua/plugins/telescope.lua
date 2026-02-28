964828b216a9b1b09f0d6a2d5e6c47280aa702b8
lfrecalde1 <lfrecalde1@espe.edu.ec>
2026-02-28 17:45:32 +0000

    updates telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,38 @@
+---@type LazySpec
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    cmd = "Telescope",
+    keys = {
+      { "<Leader>ff", "<Cmd>Telescope find_files<CR>", desc = "Find files" },
+      { "<Leader>fg", "<Cmd>Telescope live_grep<CR>", desc = "Live grep" },
+      { "<Leader>fb", "<Cmd>Telescope buffers<CR>", desc = "Buffers" },
+      { "<Leader>fh", "<Cmd>Telescope help_tags<CR>", desc = "Help" },
+    },
+    opts = function()
+      local actions = require "telescope.actions"
+      return {
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+            },
+          },
+        },
+        pickers = {
+          find_files = { hidden = true },
+        },
+      }
+    end,
+    config = function(_, opts)
+      local telescope = require "telescope"
+      telescope.setup(opts)
+      pcall(telescope.load_extension, "fzf")
+    end,
+  },
+}
